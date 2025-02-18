fd97a174df54ec7966bc27153e31a45f2e497e6d
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-18 14:36:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0; i<str.length; i++){
+		if(str[i]!==str[i+1]){
+			return str[i]
+		}else {
+			return null
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
